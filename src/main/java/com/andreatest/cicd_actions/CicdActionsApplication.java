a81a20fcd04165f5da15c3a07b187593627ff2d3
AndreaChirulescu <andrea.chirulescu@gmail.com>
2025-09-15 16:53:55 +0200

    first commit

---
+++ src/main/java/com/andreatest/cicd_actions/CicdActionsApplication.java
@@ -0,0 +1,30 @@
+package com.andreatest.cicd_actions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class CicdActionsApplication {
+
+	@GetMapping("/welcome")
+	public String welcome(){
+		return "Andrea was here";
+	}
+	
+	
+//	echo "# cicdactions" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M master
+// git remote add origin https://github.com/AndreaChirulescu/cicdactions.git
+// git push -u origin master
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdActionsApplication.class, args);
+	}
+
+}
